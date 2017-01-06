fc9eda55ae204965dd41ecf667c89a2882ce089b
Jesse McIntosh <jessemc98@hotmail.com>
2017-01-06 19:03:17 +0100

    removed unnesasary files from project and changed index.js to be entry point for component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import MotionMenu from './MotionMenu'
+import SpacedMotionMenu from './SpacedMotionMenu'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+export default MotionMenu
+export { SpacedMotionMenu }
